2a45e69e3abb1b39bb753e0b5f5e32237f3b700d
Rohit-Pandit <159272518+Rohit-Pandit@users.noreply.github.com>
2025-08-06 15:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	return (celsius*1.8 + 32).toFixed(2);
 }
 
 // Do not change the code below
-const celsius = prompt("Enter Celsius:");
-alert(toFahrenheit(Number(celsius)));
+//const celsius = prompt("Enter Celsius:");
+//alert(toFahrenheit(Number(celsius)));
